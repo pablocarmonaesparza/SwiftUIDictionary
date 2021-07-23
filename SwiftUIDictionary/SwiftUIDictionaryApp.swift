59ff6e5cddfbedc34f9e8959b30ef7e5721719c4
Pablo Carmona <pablo@moliquitwaiting.com>
2021-07-22 19:26:50 -0500

    Initial Commit

---
+++ SwiftUIDictionary/SwiftUIDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDictionaryApp.swift
+//  SwiftUIDictionary
+//
+//  Created by Pablo Carmona Esparza on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
